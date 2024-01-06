e56bf5f4f79c7a2ff79a86f3767694cd0398fd3e
digitalprintingind <54123692+digitalprintingind@users.noreply.github.com>
2024-01-06 16:07:03 +0700

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -45,7 +45,7 @@ $(function () {
         function _submitForm() {
             $.ajax({
                 'type': "post",
-                'url': "https://script.google.com/macros/s/AKfycbw83wfhMnVYVEPTcH6HgPRLjwi11BoUz2Pd5rzM47hDevYPR2LAnDkwYYBF3KJY0ltrow/exec",
+                'url': "https://formsubmit.co/csumoe@gmail.com",
                 'data': {
                     'name': $nameField.val(),
                     'email': $emailField.val(),
